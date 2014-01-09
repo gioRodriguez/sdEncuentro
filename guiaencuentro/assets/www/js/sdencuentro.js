726b2475de3a0212c737ad8f40187b0ad0d4387d
gio <gio.rodriguez88@gmail.com>
2014-01-09 12:55:51 -0600

    footer added

--- guiaencuentro/assets/www/js/sdencuentro.js
+++ guiaencuentro/assets/www/js/sdencuentro.js
@@ -6,22 +6,34 @@
 
 define([ 'js/lib/fastclick' ], function() {
 	var sdEncuentroApp = angular.module('SdEncuentro', [ 'ngRoute', 'ngTouch',
-			'ajoslin.mobile-navigate' ]);
+			'ajoslin.mobile-navigate', 'pascalprecht.translate' ]);
 
 	// routes config
-	sdEncuentroApp.config([ '$routeProvider', function($routeProvider) {
-		$routeProvider.when('/', {
-			templateUrl : 'js/app/views/HomeView.html',
-			controller : 'HomeController'
-		});
-		$routeProvider.when('/textViewer', {
-			templateUrl : 'js/app/views/TextViewerView.html',
-			controller : 'TextViewerController'
-		});
-		$routeProvider.otherwise({
-			redirectTo : '/'
-		});
-	} ]);
+	sdEncuentroApp.config([ '$routeProvider', '$translateProvider',
+			function($routeProvider, $translateProvider) {
+				$routeProvider.when('/', {
+					templateUrl : 'js/app/views/HomeView.html',
+					controller : 'HomeController'
+				});
+				$routeProvider.when('/textViewer', {
+					templateUrl : 'js/app/views/TextViewerView.html',
+					controller : 'TextViewerController'
+				});
+				$routeProvider.otherwise({
+					redirectTo : '/'
+				});
+				
+				// i18n
+				$translateProvider.translations('es', {
+					'TITLE' : 'Guia encuentro'
+				});
+				
+				$translateProvider.translations('en', {
+					'TITLE' : 'Meet guie'
+				});
+				
+				$translateProvider.preferredLanguage('es');
+			} ]);
 
 	sdEncuentroApp.initialize = function() {
 		bindEvents();
