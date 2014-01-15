6c3079ca97a8c9c0dfba9e1cc2737ab8bb0ba0f6
gio <gio.rodriguez88@gmail.com>
2014-01-14 18:38:30 -0600

    cordova plugins installed

--- guiaencuentroI/platforms/android/.staging/www/cordova_plugins.js
+++ guiaencuentroI/platforms/android/.staging/www/cordova_plugins.js
@@ -1,6 +1,13 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
@@ -13,20 +20,13 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
-        "id": "org.apache.cordova.vibration.notification",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.dialogs": "0.2.5",
-    "org.apache.cordova.vibration": "0.3.6"
+    "org.apache.cordova.vibration": "0.3.6",
+    "org.apache.cordova.dialogs": "0.2.5"
 }
 // BOTTOM OF METADATA
 });
