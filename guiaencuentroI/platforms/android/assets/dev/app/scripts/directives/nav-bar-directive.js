f694ccf175d5eebedb14ca7c8f6db9d1016037c6
gio <gio.rodriguez88@gmail.com>
2014-06-27 18:10:31 -0500

    uglify issue in directive controller fixed

--- guiaencuentroI/platforms/android/assets/dev/app/scripts/directives/nav-bar-directive.js
+++ guiaencuentroI/platforms/android/assets/dev/app/scripts/directives/nav-bar-directive.js
@@ -1,26 +1,32 @@
 (function() {
   'use strict';
-  
-  var illyumNavBarDirective = function() {
-  
-    return{
-      transclude : true,
-      restrict : 'EA',
-      template : '<div style="bottom: -0.5rem;" class="topcoat-navigation-bar ng-class:{highContrast: constratEnabled}"> <span ng-transclude></span> </div>',
-      scope : {
-        constratEnabled : '=isConstratEnabled',
-        itemSize : '@itemsSize'
-      },
-      controller : function($scope) {
-        this.getItemSize = function() {
-          return $scope.itemSize;
-        };
-      },
-      link : function(scope, element, attrs) {
-        
+
+  var illyumNavBarDirective =
+    function() {
+
+      return {
+        transclude : true,
+        restrict : 'EA',
+        template : '<div style="bottom: -0.5rem;" class="topcoat-navigation-bar ng-class:{highContrast: constratEnabled}"> <span ng-transclude></span> </div>',
+        scope : {
+          constratEnabled : '=isConstratEnabled',
+          itemSize : '@itemsSize'
+        },
+        controller : [
+          '$scope',
+          function($scope) {
+            this.getItemSize = function() {
+              return $scope.itemSize;
+            };
+          }
+        ],
+        link : function(scope, element, attrs) {
+
+        }
       }
-    }
-  };
-  
-  angular.module('guiaEncuentroApp').directive('illyumNavBar', illyumNavBarDirective);
+    };
+
+  angular.module('guiaEncuentroApp').directive('illyumNavBar', [
+    illyumNavBarDirective
+  ]);
 })();
--- guiaencuentroI/platforms/android/assets/dev/app/scripts/directives/nav-bar-item-directive.js
+++ guiaencuentroI/platforms/android/assets/dev/app/scripts/directives/nav-bar-item-directive.js
@@ -20,5 +20,5 @@
     };
   };
   
-  angular.module('guiaEncuentroApp').directive('illyumNavBarItem', illyumNavBarItemDirective);
+  angular.module('guiaEncuentroApp').directive('illyumNavBarItem', [illyumNavBarItemDirective]);
 })();
