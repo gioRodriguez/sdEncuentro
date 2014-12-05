4e41290338bc0f4156234dbf3dac38542617479f
gio.rodriguez88@gmail.com <gio.rodriguez88@gmail.com>
2014-12-05 17:23:06 -0600

    plan encuentro 2015 preparation stage

--- guiaencuentroI/platforms/android/src/com/sdencuentro/guia/plugins/GetTextPlugin.java
+++ guiaencuentroI/platforms/android/src/com/sdencuentro/guia/plugins/GetTextPlugin.java
@@ -4,7 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.cordova.CallbackContext;
@@ -16,6 +18,12 @@ import android.content.res.AssetManager;
 import android.util.Log;
 
 public class GetTextPlugin extends CordovaPlugin {
+	private static final int DEFAULT_YEAR = 2014;
+	private static final Set<Integer> AVAILABLE_YEARS = new HashSet<Integer>();
+	static{
+		AVAILABLE_YEARS.add(DEFAULT_YEAR);
+		AVAILABLE_YEARS.add(2015);
+	}
 	
 	@Override
 	public boolean execute(String action, final JSONArray args,
@@ -31,11 +39,18 @@ public class GetTextPlugin extends CordovaPlugin {
 			public void run() {
 				try {
 					//2014-Febrero-01
-					String dateSelected = args.getString(0);
+					String dateSelected = args.getString(0);													
+					
 					String date[] = dateSelected.split("-");
-					String month = date[1].toLowerCase(Locale.ENGLISH);
-					int day = Integer.parseInt(date[2]);
-					String file = String.format(Locale.ENGLISH, "www/texts/%s.gz.js", month);
+					
+					String month = date[1].toLowerCase(Locale.ENGLISH);					
+					int day = Integer.parseInt(date[2]);					
+					int year = Integer.parseInt(date[0]);
+					if(!AVAILABLE_YEARS.contains(year)){
+						year = DEFAULT_YEAR;
+					}
+					
+					String file = String.format(Locale.ENGLISH, "www/texts/%s/%s.gz.js", year, month);
 					String content = loadFile(file);
 					
 					String [] texts = content.split("\n");
